8d38986c7b1a77e2bed40f484f6933fc4fc58e22
Calum29d <calumdavies08@gmail.com>
2026-06-06 22:31:43 +0100

    Added a back button to the faces page and just a simple start

--- app/faces.tsx
+++ app/faces.tsx
@@ -1,10 +1,59 @@
-import { Text, View } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useRouter } from "expo-router";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function FacesScale() {
-    
+
+    const  router = useRouter();
+
     return (
-        <View>
-            <Text>Wong-Baker FACES Scale</Text>
-        </View>
+        <SafeAreaView style = {styles.safeArea}>
+            <ScrollView style = {styles.scrollContent}>
+                <Pressable style = {styles.backArrow}
+                    onPress = {() => router.back()}>
+                        <FontAwesome name="arrow-left" size={30} color="#005EB8" />
+                    </Pressable>
+                { /* Header */}
+                <View style = {styles.header}>
+
+                    <Text style = {styles.title}>Wong-Baker {"\n"} FACES Scale</Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+
+    backArrow: {
+        marginLeft: 20,
+    },
+
+
+    title: {
+        fontSize: 28,
+        fontWeight: "bold",
+        fontFamily: "Inter_700Bold",
+        color: "#0e0c0c",
+        textAlign: "center",
+        marginTop: 20,
+
+    },
+
+    header: {
+        marginBottom: 24,
+        backgroundColor: "#F8FAFC",
+        alignItems: "center",
+  },
+
+    safeArea: {
+        flex: 1,
+        backgroundColor: "#F8FAFC",
+  },
+
+    scrollContent: {
+        backgroundColor: "#F8FAFC",
+  },
+
+})
