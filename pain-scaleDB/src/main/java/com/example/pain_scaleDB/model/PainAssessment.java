37ed2ece724e230a748e3f50908e057168c6b609
Calum29d <calumdavies08@gmail.com>
2026-07-05 21:52:24 +0100

    created the pain asessment entity model

---
+++ pain-scaleDB/src/main/java/com/example/pain_scaleDB/model/PainAssessment.java
@@ -0,0 +1,76 @@
+package com.example.pain_scaleDB.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+/*This class holds the datashape for a pain assessment that the patient has had */
+@Entity
+@Table(name = "pain_assessments")
+public class PainAssessment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /*saves the date and time of when the assessement was made */
+    @Column(nullable = false)
+    private LocalDateTime assessedAt;
+
+    private String scaleType;
+    private int score;
+
+    /*many pain assessments can point to one patient */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id", nullable = false)
+    private Patient patient;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getAssessedAt() {
+        return assessedAt;
+    }
+
+    public void setAssessedAt(LocalDateTime assessedAt) {
+        this.assessedAt = assessedAt;
+    }
+
+    public String getScaleType() {
+        return scaleType;
+    }
+
+    public void setScaleType(String scaleType) {
+        this.scaleType = scaleType;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+    
+}
---
+++ pain-scaleDB/src/main/java/com/example/pain_scaleDB/service/PainAsessmentService.java
@@ -0,0 +1,5 @@
+package com.example.pain_scaleDB.service;
+
+public interface PainAsessmentService {
+
+}
